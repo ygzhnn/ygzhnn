6d7b51cc44bb12e760a301cec2d2c0c746a8f66e
Yağız Han <149168571+ygzhnn@users.noreply.github.com>
2024-04-03 15:19:36 +0300

    Add files via upload

---
+++ FlaskWeb/website/auth.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 character.', category='error')
+
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+
+        elif password1 < 7:
+            flash('Password must be at least 7 characters.', category='error')
+
+        else:
+            #add user to database
+            flash('Account created!', category='success')
+            
+
+    return render_template("sign_up.html")
